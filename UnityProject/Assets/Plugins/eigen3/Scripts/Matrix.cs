e218b4e486ec3d65d12f72acd192cddb8899659f
leebissessar5 <leebissessar@gmail.com>
2023-10-14 13:15:09 -0400

    refactor(eigen): change class name to Matrix

--- UnityProject/Assets/Plugins/eigen3/Scripts/Matrix.cs
+++ UnityProject/Assets/Plugins/eigen3/Scripts/Matrix.cs
@@ -1,18 +1,18 @@
 using System;
 using System.Runtime.InteropServices;
 
-public class EigenMatrix : IDisposable
+public class Matrix : IDisposable
 {
     private IntPtr _matrixPtr;
 
     [DllImport("EigenWrapper.dll")]
-    private static extern IntPtr CreateMatrix(int rows, int cols, double[] data);
+    private static extern IntPtr CreateMatrix(int rows, int cols, float[] data);
 
     [DllImport("EigenWrapper.dll")]
     private static extern void DeleteMatrix(IntPtr matrixPtr);
 
     [DllImport("EigenWrapper.dll")]
-    private static extern void MultiplyMatrices(IntPtr matrix1, IntPtr matrix2, double[] result);
+    private static extern void MultiplyMatrices(IntPtr matrix1, IntPtr matrix2, float[] result);
 
     [DllImport("EigenWrapper.dll")]
     private static extern int GetRowCount(IntPtr matrixPtr);
@@ -23,16 +23,16 @@ public class EigenMatrix : IDisposable
     [DllImport("EigenWrapper.dll")]
     private static extern float GetElement(IntPtr matrixPtr, int row, int col);
 
-    public EigenMatrix(int rows, int cols, double[] data)
+    public Matrix(int rows, int cols, float[] data)
     {
         _matrixPtr = CreateMatrix(rows, cols, data);
     }
 
-    public EigenMatrix Multiply(EigenMatrix other)
+    public Matrix Multiply(Matrix other)
     {
-        double[] resultData = new double[RowCount * other.ColCount];
+        float[] resultData = new float[RowCount * other.ColCount];
         MultiplyMatrices(_matrixPtr, other._matrixPtr, resultData);
-        return new EigenMatrix(RowCount, other.ColCount, resultData);
+        return new Matrix(RowCount, other.ColCount, resultData);
     }
 
     public int RowCount => GetRowCount(_matrixPtr);
