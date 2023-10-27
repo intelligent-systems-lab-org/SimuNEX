b2d4ab3e3574268a6d843b53610f7629ae3437d4
leebissessar5 <leebissessar@gmail.com>
2023-10-26 20:59:03 -0400

    feat(dynamics): add quadratic drag.

---
+++ UnityProject/Assets/Scripts/Derived/Dynamics/Force/QuadraticDrag.cs
@@ -0,0 +1,38 @@
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Implementation of quadratic drag where the force depends on the square of the velocity of the <see cref="RigidBody"/>.
+/// </summary>
+public class QuadraticDrag : Force
+{
+    /// <summary>
+    /// Drag coefficients defined as six numbers, where each applies to a DOF.
+    /// </summary>
+    public Vector6DOF dragCoefficients;
+
+    public override void ApplyForce()
+    {
+        rb.AddForce(-1 * rb.velocity.Apply(v => Mathf.Abs(v) * v) * dragCoefficients);
+    }
+}
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(QuadraticDrag))]
+public class QuadraticDragEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        QuadraticDrag quadDrag = (QuadraticDrag)target;
+
+        if (quadDrag.gameObject.GetComponents<QuadraticDrag>().Length > 1)
+        {
+            EditorGUILayout.HelpBox("Only one QuadraticDrag component can be added to the GameObject.", MessageType.Error);
+            return;
+        }
+
+        DrawDefaultInspector();
+    }
+}
+#endif
