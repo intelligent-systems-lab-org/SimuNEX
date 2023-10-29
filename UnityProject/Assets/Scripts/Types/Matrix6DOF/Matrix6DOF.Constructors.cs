dbc8f1840a5b366a931f1030795a29f905c77e42
leebissessar5 <leebissessar@gmail.com>
2023-10-29 12:13:26 -0400

    refactor(SimuNEX): remove unused namespaces. In Matrix6DOF.Conversions and Constructors.

--- UnityProject/Assets/Scripts/Types/Matrix6DOF/Matrix6DOF.Constructors.cs
+++ UnityProject/Assets/Scripts/Types/Matrix6DOF/Matrix6DOF.Constructors.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 /// <summary>
 /// Represents a 6 x 6 matrix used in dynamics calculations.
