464cd5e906356300082188a91d57e2614d33cb27
leebissessar5 <leebissessar@gmail.com>
2023-11-02 12:17:02 -0400

    feat(SimuNEX): add automated naming for Ideal6DOFMotionSensor.

--- UnityProject/Assets/Scripts/Derived/Sensors/Ideal6DOFMotionSensor.cs
+++ UnityProject/Assets/Scripts/Derived/Sensors/Ideal6DOFMotionSensor.cs
@@ -5,6 +5,26 @@ namespace SimuNEX
     /// </summary>
     public class Ideal6DOFMotionSensor : Sensor
     {
+        /// <summary>
+        /// Default output names for <see cref="Ideal6DOFMotionSensor"/>.
+        /// </summary>
+        private static readonly string[] OutputLabels = new string[]
+        {
+            "X Velocity",
+            "Z Velocity",
+            "Y Velocity",
+            "X Angular Velocity",
+            "Z Angular Velocity",
+            "Y Angular Velocity",
+            "W Angular Position",
+            "X Angular Position",
+            "Z Angular Position",
+            "Y Angular Position",
+            "X Position",
+            "Z Position",
+            "Y Position"
+        };
+
         protected override void Initialize()
         {
             outputs = () => new float[]
@@ -23,6 +43,26 @@ namespace SimuNEX
                 rigidBody.position.z,
                 rigidBody.position.y
             };
+
+            outputNames = GenerateOutputNames();
+        }
+
+        /// <summary>
+        /// Generates output names by prepending the name of the <see cref="RigidBody"/>
+        /// to the default values.
+        /// </summary>
+        /// <returns>A string array containing output names.</returns>
+        private string[] GenerateOutputNames()
+        {
+            string rigidBodyName = rigidBody.gameObject.name;
+            string[] generatedNames = new string[OutputLabels.Length];
+
+            for (int i = 0; i < OutputLabels.Length; i++)
+            {
+                generatedNames[i] = $"{rigidBodyName} {OutputLabels[i]}";
+            }
+
+            return generatedNames;
         }
 
         protected void OnValidate()
