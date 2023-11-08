c24861dd23e55f49975fba8f48fc4e9955ba0c56
leebissessar5 <leebissessar@gmail.com>
2023-11-08 17:20:41 -0400

    refactor(SImuNEX): split files. attributes and object extensions. Add PR template.

--- UnityProject/Assets/Scripts/Core/Attributes.cs
+++ UnityProject/Assets/Scripts/Core/Attributes.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Linq;
-using System.Reflection;
 using UnityEngine;
 
 namespace SimuNEX
@@ -35,24 +33,5 @@ namespace SimuNEX
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
     public class Faultable : Attribute
     {
-    }
-
-    public static class ObjectExtensions
-    {
-        public static FieldInfo[] GetFieldsWithAttribute<T>(this object obj) where T : Attribute
-        {
-            return obj.GetType()
-                .GetFields(BindingFlags.Public | BindingFlags.Instance)
-                .Where(f => Attribute.IsDefined(f, typeof(T)))
-                .ToArray();
-        }
-
-        public static PropertyInfo[] GetPropertiesWithAttribute<T>(this object obj) where T : Attribute
-        {
-            return obj.GetType()
-                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                .Where(p => Attribute.IsDefined(p, typeof(T)))
-                .ToArray();
-        }
     }
 }
