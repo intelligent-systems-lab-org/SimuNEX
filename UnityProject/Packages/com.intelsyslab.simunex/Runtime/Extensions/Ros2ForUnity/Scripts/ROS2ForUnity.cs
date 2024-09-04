d509a4d6f8857dd9d29b97c891f5fa50ddbd56a6
leebissessar5 <leebissessar@gmail.com>
2024-09-04 12:59:48 -0400

    fix(SimuNEX): Fix ROS2 plugin path error.

--- UnityProject/Packages/com.intelsyslab.simunex/Runtime/Extensions/Ros2ForUnity/Scripts/ROS2ForUnity.cs
+++ UnityProject/Packages/com.intelsyslab.simunex/Runtime/Extensions/Ros2ForUnity/Scripts/ROS2ForUnity.cs
@@ -13,257 +13,252 @@
 // limitations under the License.
 
 using System;
-using System.IO;
 using System.Collections.Generic;
-using UnityEngine;
+using System.IO;
 using UnityEditor;
+using UnityEngine;
 
 namespace ROS2
 {
-
-/// <summary>
-/// An internal class responsible for handling checking, proper initialization and shutdown of ROS2cs,
-/// </summary>
-internal class ROS2ForUnity
-{
-    private static bool isInitialized = false;
-    private static string ros2ForUnityAssetFolderName = "Ros2ForUnity";
-
-    enum Platform
-    {
-        Windows,
-        Linux
-    }
-
-    private Platform GetOS()
-    {
-        if (Application.platform == RuntimePlatform.LinuxEditor || Application.platform == RuntimePlatform.LinuxPlayer)
-        {
-            return Platform.Linux;
-        }
-        else if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer)
-        {
-            return Platform.Windows;
-        }
-        throw new System.NotSupportedException("Only Linux and Windows are supported");
-    }
-
-    private bool InEditor() {
-        return Application.isEditor;
-    }
-
-    private string GetOSName()
-    {
-        switch (GetOS())
-        {
-            case Platform.Linux:
-                return "Linux";
-            case Platform.Windows:
-                return "Windows";
-            default:
-                throw new System.NotSupportedException("Only Linux and Windows are supported");
-        }
-    }
-
-    private string GetEnvPathVariableName()
-    {
-      string envVariable = "LD_LIBRARY_PATH";
-      if (GetOS() == Platform.Windows)
-      {
-          envVariable = "PATH";
-      }
-      return envVariable;
-    }
-
-    private string GetEnvPathVariableValue()
-    {
-        return Environment.GetEnvironmentVariable(GetEnvPathVariableName());
-    }
-
-    private string GetPluginPath()
-    {
-        char separator = Path.DirectorySeparatorChar;
-        string pluginPath = Application.dataPath;
-
-        pluginPath += separator + "Extensions";
-
-        if (InEditor()) {
-            // Ros2ForUnity is now inside the Plugins folder
-            pluginPath += separator + ros2ForUnityAssetFolderName;
-        }
-
-        if (InEditor()) {
-            pluginPath += separator + GetOSName();
-        }
-
-        if (InEditor() || GetOS() == Platform.Windows)
-        {
-           pluginPath += separator + "x86_64";
-        }
-
-        if (GetOS() == Platform.Windows)
-        {
-           pluginPath = pluginPath.Replace("/", "\\");
-        }
-
-        return pluginPath;
-    }
-
     /// <summary>
-    /// Function responsible for setting up of environment paths for standalone builds
+    /// An internal class responsible for handling checking, proper initialization and shutdown of ROS2cs,
     /// </summary>
-    /// <description>
-    /// Note that on Linux, LD_LIBRARY_PATH as used for dlopen() is determined on process start and this change won't
-    /// affect it. Ros2 looks for rmw implementation based on this variable (independently) and the change
-    /// is effective for this process, however rmw implementation's dependencies itself are loaded by dynamic linker
-    /// anyway so setting  it for Linux is pointless.
-    /// </description>
-    private void SetEnvPathVariable()
+    internal class ROS2ForUnity
     {
-        string currentPath = GetEnvPathVariableValue();
-        string pluginPath = GetPluginPath();
+        private static bool isInitialized;
 
-        char envPathSep = ':';
-        if (GetOS() == Platform.Windows)
+        private const string ros2ForUnityAssetFolderName = "Ros2ForUnity";
+
+        private enum Platform
         {
-            envPathSep = ';';
+            Windows,
+            Linux
         }
 
-        Environment.SetEnvironmentVariable(GetEnvPathVariableName(), pluginPath + envPathSep + currentPath);
-    }
-
-    /// <summary>
-    /// Check if the ros version is supported, only applicable to non-standalone plugin versions
-    /// (i. e. without ros2 libraries included in the plugin).
-    /// </summary>
-    private void CheckROSVersionSourced()
-    {
-        string currentVersion = Environment.GetEnvironmentVariable("ROS_DISTRO");
-        List<string> supportedVersions = new List<string>() { "foxy", "galactic" };
-        var supportedVersionsString = String.Join(", ", supportedVersions);
-        if (string.IsNullOrEmpty(currentVersion))
+        private Platform GetOS()
         {
-            string errMessage = "No ROS environment sourced. You need to source your ROS2 "
-                + supportedVersionsString
-                + " environment before launching Unity (ROS_DISTRO env variable not found)";
-            Debug.LogError(errMessage);
+            if (Application.platform is RuntimePlatform.LinuxEditor or RuntimePlatform.LinuxPlayer)
+            {
+                return Platform.Linux;
+            }
+            else if (Application.platform is RuntimePlatform.WindowsEditor or RuntimePlatform.WindowsPlayer)
+            {
+                return Platform.Windows;
+            }
+
+            throw new NotSupportedException("Only Linux and Windows are supported");
+        }
+
+        private bool InEditor()
+        {
+            return Application.isEditor;
+        }
+
+        private string GetOSName()
+        {
+            return GetOS() switch
+            {
+                Platform.Linux => "Linux",
+                Platform.Windows => "Windows",
+                _ => throw new NotSupportedException("Only Linux and Windows are supported"),
+            };
+        }
+
+        private string GetEnvPathVariableName()
+        {
+            string envVariable = "LD_LIBRARY_PATH";
+            if (GetOS() == Platform.Windows)
+            {
+                envVariable = "PATH";
+            }
+
+            return envVariable;
+        }
+
+        private string GetEnvPathVariableValue()
+        {
+            return Environment.GetEnvironmentVariable(GetEnvPathVariableName());
+        }
+
+        private string GetPluginPath()
+        {
+            // Assuming your package is in the Packages folder
+            string packagePath = Path.Combine(Application.dataPath, "..", "Packages", "com.intelsyslab.simunex", "Runtime", "Extensions");
+
+            // Now handle Ros2ForUnity relative to packagePath
+            if (InEditor())
+            {
+                packagePath = Path.Combine(packagePath, ros2ForUnityAssetFolderName);
+            }
+
+            if (InEditor())
+            {
+                packagePath = Path.Combine(packagePath, GetOSName());
+            }
+
+            if (InEditor() || GetOS() == Platform.Windows)
+            {
+                packagePath = Path.Combine(packagePath, "x86_64");
+            }
+
+            if (GetOS() == Platform.Windows)
+            {
+                packagePath = packagePath.Replace("/", "\\");
+            }
+
+            return packagePath;
+        }
+
+        /// <summary>
+        /// Function responsible for setting up of environment paths for standalone builds
+        /// </summary>
+        /// <description>
+        /// Note that on Linux, LD_LIBRARY_PATH as used for dlopen() is determined on process start and this change won't
+        /// affect it. Ros2 looks for rmw implementation based on this variable (independently) and the change
+        /// is effective for this process, however rmw implementation's dependencies itself are loaded by dynamic linker
+        /// anyway so setting  it for Linux is pointless.
+        /// </description>
+        private void SetEnvPathVariable()
+        {
+            string currentPath = GetEnvPathVariableValue();
+            string pluginPath = GetPluginPath();
+
+            char envPathSep = ':';
+            if (GetOS() == Platform.Windows)
+            {
+                envPathSep = ';';
+            }
+
+            Environment.SetEnvironmentVariable(GetEnvPathVariableName(), pluginPath + envPathSep + currentPath);
+        }
+
+        /// <summary>
+        /// Check if the ros version is supported, only applicable to non-standalone plugin versions
+        /// (i. e. without ros2 libraries included in the plugin).
+        /// </summary>
+        /// <exception cref="InvalidOperationException"></exception>
+        /// <exception cref="NotSupportedException"></exception>
+        private void CheckROSVersionSourced()
+        {
+            string currentVersion = Environment.GetEnvironmentVariable("ROS_DISTRO");
+            List<string> supportedVersions = new() { "foxy", "galactic" };
+            string supportedVersionsString = string.Join(", ", supportedVersions);
+            if (string.IsNullOrEmpty(currentVersion))
+            {
+                string errMessage = "No ROS environment sourced. You need to source your ROS2 "
+                    + supportedVersionsString
+                    + " environment before launching Unity (ROS_DISTRO env variable not found)";
+                Debug.LogError(errMessage);
 #if UNITY_EDITOR
-            EditorApplication.isPlaying = false;
-            throw new System.InvalidOperationException(errMessage);
+                EditorApplication.isPlaying = false;
+                throw new System.InvalidOperationException(errMessage);
 #else
             const int ROS_NOT_SOURCED_ERROR_CODE = 33;
             Application.Quit(ROS_NOT_SOURCED_ERROR_CODE);
 #endif
-        }
+            }
 
-        if (!supportedVersions.Contains(currentVersion))
-        {
-            string errMessage = "Currently sourced ROS version differs from supported one. Sourced: "
-                + currentVersion
-                + ", supported: "
-                + supportedVersionsString
-                + ".";
-            Debug.LogError(errMessage);
+            if (!supportedVersions.Contains(currentVersion))
+            {
+                string errMessage = "Currently sourced ROS version differs from supported one. Sourced: "
+                    + currentVersion
+                    + ", supported: "
+                    + supportedVersionsString
+                    + ".";
+                Debug.LogError(errMessage);
 #if UNITY_EDITOR
-            EditorApplication.isPlaying = false;
-            throw new System.NotSupportedException(errMessage);
+                EditorApplication.isPlaying = false;
+                throw new System.NotSupportedException(errMessage);
 #else
             const int ROS_BAD_VERSION_CODE = 34;
             Application.Quit(ROS_BAD_VERSION_CODE);
 #endif
-        }
-        Debug.Log("Running with a supported ROS 2 version: " + currentVersion);
-    }
+            }
 
-    private void RegisterCtrlCHandler()
-    {
+            Debug.Log("Running with a supported ROS 2 version: " + currentVersion);
+        }
+
+        private void RegisterCtrlCHandler()
+        {
 #if ENABLE_MONO
-        // Il2CPP build does not support Console.CancelKeyPress currently
-        Console.CancelKeyPress += (sender, eventArgs) => {
-            eventArgs.Cancel = true;
-            DestroyROS2ForUnity();
-        };
+            // Il2CPP build does not support Console.CancelKeyPress currently
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                DestroyROS2ForUnity();
+            };
 #endif
-    }
-
-    private void ConnectLoggers()
-    {
-        Ros2csLogger.setCallback(LogLevel.ERROR, Debug.LogError);
-        Ros2csLogger.setCallback(LogLevel.WARNING, Debug.LogWarning);
-        Ros2csLogger.setCallback(LogLevel.INFO, Debug.Log);
-        Ros2csLogger.setCallback(LogLevel.DEBUG, Debug.Log);
-        Ros2csLogger.LogLevel = LogLevel.WARNING;
-    }
-
-    internal ROS2ForUnity()
-    {
-        // TODO: Find a way to determine whether we run standalone build
-        if (GetOS() == Platform.Windows) {
-            // Windows version can run standalone, modifies PATH to ensure all plugins visibility
-            SetEnvPathVariable();
-        } else {
-            // Linux version needs to have ros2 sourced, which is checked here. It also loads plugins by absolute path
-            // since LD_LIBRARY_PATH cannot be set dynamically within the process for dlopen() which is used under the hood.
-            // Since libraries are built with -rpath=".", dependencies will be correcly located within plugins directory
-            CheckROSVersionSourced();
-            ROS2.GlobalVariables.absolutePath = GetPluginPath() + "/";
         }
-        ConnectLoggers();
-        Ros2cs.Init();
-        RegisterCtrlCHandler();
+
+        private void ConnectLoggers()
+        {
+            Ros2csLogger.setCallback(LogLevel.ERROR, Debug.LogError);
+            Ros2csLogger.setCallback(LogLevel.WARNING, Debug.LogWarning);
+            Ros2csLogger.setCallback(LogLevel.INFO, Debug.Log);
+            Ros2csLogger.setCallback(LogLevel.DEBUG, Debug.Log);
+            Ros2csLogger.LogLevel = LogLevel.WARNING;
+        }
+
+        internal ROS2ForUnity()
+        {
+            // TODO: Find a way to determine whether we run standalone build
+            if (GetOS() == Platform.Windows)
+            {
+                // Windows version can run standalone, modifies PATH to ensure all plugins visibility
+                SetEnvPathVariable();
+            }
+            else
+            {
+                // Linux version needs to have ros2 sourced, which is checked here. It also loads plugins by absolute path
+                // since LD_LIBRARY_PATH cannot be set dynamically within the process for dlopen() which is used under the hood.
+                // Since libraries are built with -rpath=".", dependencies will be correcly located within plugins directory
+                CheckROSVersionSourced();
+                ROS2.GlobalVariables.absolutePath = GetPluginPath() + "/";
+            }
+
+            ConnectLoggers();
+            Ros2cs.Init();
+            RegisterCtrlCHandler();
 
 #if UNITY_EDITOR
-        EditorApplication.playModeStateChanged += this.EditorPlayStateChanged;
-        EditorApplication.quitting += this.DestroyROS2ForUnity;
+            EditorApplication.playModeStateChanged += EditorPlayStateChanged;
+            EditorApplication.quitting += DestroyROS2ForUnity;
 #endif
-        isInitialized = true;
-    }
-
-    private static void ThrowIfUninitialized(string callContext)
-    {
-        if (!isInitialized)
-        {
-            throw new InvalidOperationException("Ros2 For Unity is not initialized, can't " + callContext);
+            isInitialized = true;
         }
-    }
 
-    /// <summary>
-    /// Check if ROS2 module is properly initialized and no shutdown was called yet
-    /// </summary>
-    /// <returns>The state of ROS2 module. Should be checked before attempting to create or use pubs/subs</returns>
-    public bool Ok()
-    {
-        if (!isInitialized)
+        /// <summary>
+        /// Check if ROS2 module is properly initialized and no shutdown was called yet
+        /// </summary>
+        /// <returns>The state of ROS2 module. Should be checked before attempting to create or use pubs/subs</returns>
+        public bool Ok()
         {
-            return false;
+            return isInitialized && Ros2cs.Ok();
         }
-        return Ros2cs.Ok();
-    }
 
-    internal void DestroyROS2ForUnity()
-    {
-        if (isInitialized)
+        internal void DestroyROS2ForUnity()
         {
-            Debug.Log("Shutting down Ros2 For Unity");
-            Ros2cs.Shutdown();
-            isInitialized = false;
+            if (isInitialized)
+            {
+                Debug.Log("Shutting down Ros2 For Unity");
+                Ros2cs.Shutdown();
+                isInitialized = false;
+            }
         }
-    }
 
-    ~ROS2ForUnity()
-    {
-        DestroyROS2ForUnity();
-    }
-
-#if UNITY_EDITOR
-    void EditorPlayStateChanged(PlayModeStateChange change)
-    {
-        if (change == PlayModeStateChange.ExitingPlayMode)
+        ~ROS2ForUnity()
         {
             DestroyROS2ForUnity();
         }
-    }
-#endif
-}
 
+#if UNITY_EDITOR
+        private void EditorPlayStateChanged(PlayModeStateChange change)
+        {
+            if (change == PlayModeStateChange.ExitingPlayMode)
+            {
+                DestroyROS2ForUnity();
+            }
+        }
+#endif
+    }
 }  // namespace ROS2
